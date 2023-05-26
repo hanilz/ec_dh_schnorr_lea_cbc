ff24bd694de89a6b147d0ab09d8bf3042b1a08a3
doleval013 <100090773+doleval013@users.noreply.github.com>
2023-05-26 12:18:41 +0300

    bad

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ from references.schnorr_sig.schnorr_lib import sha256
 from references.secured_channel.diffie_hellman import *
 from references.schnorr_sig import schnorr_lib as sl
 
+import json
+import binascii
 
 def point_to_bytes(point: Point):
     x_bytes = point.x.n.to_bytes(16, 'big')
@@ -87,17 +89,27 @@ def main():
     print("Alice want to send encrypt message to Bob using LEA in CBC mode with Elliptic Curve Diffie-Hellman key")
     print("Alice generating a private and public Elliptic Curve Diffie-Hellman key")
 
-    F = FiniteField(3851, 1)
-    curve = EllipticCurve(a=F(324), b=F(1287))
-    base_point = Point(curve, F(920), F(303))
-
+    #F = FiniteField(3851, 1)
+    #curve = EllipticCurve(a=F(324), b=F(1287))
+    #base_point = Point(curve, F(920), F(303))
+    import references.schnorr_sig.create_keypair as ckp
+    import references.schnorr_sig.schnorr_lib as sl
+    ckp.main()
     # Alice's private and public keys
-    alice_secret_key = generateSecretKey(256)
-    alice_public_key = sendDH(alice_secret_key, base_point, lambda x: x)
+    #alice_secret_key = generateSecretKey(256)
+    #alice_public_key = sendDH(alice_secret_key, base_point, lambda x: x)
+    filename = "users.json"
+    with open(filename, "r") as file:
+        data = json.load(file)
+    alice_secret_key = data['users'][0]['privateKey']
+    alice_public_key = Point(tuple(data['users'][0]['publicKey']))
 
     # Bob's private and public keys
-    bob_secret_key = generateSecretKey(256)
-    bob_public_key = sendDH(bob_secret_key, base_point, lambda x: x)
+    #bob_secret_key = generateSecretKey(256)
+    #bob_public_key = sendDH(bob_secret_key, base_point, lambda x: x)
+
+    bob_secret_key = data['users'][1]['privateKey']
+    bob_public_key = tuple(data['users'][1]['publicKey'])
 
     # Shared keys
     sharedSecret1 = receiveDH(bob_secret_key, lambda: alice_public_key)
@@ -106,8 +118,9 @@ def main():
     print("Alice encrypts the message with LEA in CBC mode symmetric encryption")
     mp3_filename = r"cha_cha_cha.mp3"
 
-    with open(mp3_filename, 'rb') as f_mp3:
-        pt = f_mp3.read()
+    #with open(mp3_filename, 'rb') as f_mp3:
+    #    pt = f_mp3.read()
+    pt=mp3_filename
 
     #a random 128 bit initial vector
     iv = base64.b16encode(random.getrandbits(128).to_bytes(16, byteorder='little'))
@@ -119,13 +132,18 @@ def main():
 
     print("Alice signs with her secret key using schnorr signature")
     ct_bytes = sha256(ct)
-    sig = sl.schnorr_sign(ct_bytes, alice_secret_key)
+    #changed int.to_bytes(alice_secret_key)
+
+    hex_string =hex(alice_secret_key)
+    byte_string = bytes.fromhex(hex_string[2:])              #removed 0x
+    byte_string=binascii.hexlify(byte_string).decode('utf-8')#convert the byte string to its hexadecimal representation
+    sig = sl.schnorr_sign(ct_bytes, byte_string)
 
     print("Alice sends sig and ct to Bob")
     print("Bob verifies the signature")
-    s = str(alice_public_key.x.n)
-    pubkey_bytes = point_to_bytes(alice_public_key)
-    verify = sl.schnorr_verify(ct_bytes, pubkey_bytes, sig)
+    #3s = str(alice_public_key.x.n)
+    alice_public_key_bytes = point_to_bytes(alice_public_key)
+    verify = sl.schnorr_verify(ct_bytes, alice_public_key_bytes, sig)
     if verify:
         print("The signature is VALID for this message and this public key")
         print("Bob decrypts the message that Alice sent")
