80b72f7122ce5b1fd17fd947e36a86071a3078d2
YanaRaitsin <yanaraitsin4@gmail.com>
2023-05-26 17:36:39 +0300

    changed the p, n, G values and still nothing works

--- test.py
+++ test.py
@@ -7,10 +7,12 @@ import random
 
 from references.schnorr_sig.schnorr_lib import sha256
 from references.secured_channel.diffie_hellman import *
-from references.schnorr_sig import schnorr_lib as sl
+from references.schnorr_sig import schnorr_lib as sl, schnorr_lib
 
 import json
 import binascii
+import references.schnorr_sig.create_keypair as ckp
+import references.schnorr_sig.schnorr_lib as sl
 
 def point_to_bytes(point: Point):
     x_bytes = point.x.n.to_bytes(16, 'big')
@@ -89,43 +91,36 @@ def main():
     print("Alice want to send encrypt message to Bob using LEA in CBC mode with Elliptic Curve Diffie-Hellman key")
     print("Alice generating a private and public Elliptic Curve Diffie-Hellman key")
 
-    #F = FiniteField(3851, 1)
-    #curve = EllipticCurve(a=F(324), b=F(1287))
     #base_point = Point(curve, F(920), F(303))
-    import references.schnorr_sig.create_keypair as ckp
-    import references.schnorr_sig.schnorr_lib as sl
-    ckp.main()
-    # Alice's private and public keys
-    #alice_secret_key = generateSecretKey(256)
-    #alice_public_key = sendDH(alice_secret_key, base_point, lambda x: x)
+    # [0] - Alice's public key, [1] Bob's public key
+    public_keys_list = ckp.create_keypair()
+
     filename = "users.json"
     with open(filename, "r") as file:
         data = json.load(file)
     alice_secret_key = data['users'][0]['privateKey']
-    alice_public_key = Point(tuple(data['users'][0]['publicKey']))
-
-    # Bob's private and public keys
-    #bob_secret_key = generateSecretKey(256)
-    #bob_public_key = sendDH(bob_secret_key, base_point, lambda x: x)
+    alice_public_key = data['users'][0]['publicKey']
 
     bob_secret_key = data['users'][1]['privateKey']
-    bob_public_key = tuple(data['users'][1]['publicKey'])
+    bob_public_key = data['users'][1]['publicKey']
 
     # Shared keys
-    sharedSecret1 = receiveDH(bob_secret_key, lambda: alice_public_key)
-    shared_secret_key_alice = receiveDH(alice_secret_key, lambda: bob_public_key)
+    shared_secret_key_bob = receiveDH(bob_secret_key, lambda: public_keys_list[0]) # 0 - Alice, 1 - Bob
+    shared_secret_key_alice = receiveDH(alice_secret_key, lambda: public_keys_list[1])
 
     print("Alice encrypts the message with LEA in CBC mode symmetric encryption")
     mp3_filename = r"cha_cha_cha.mp3"
 
-    #with open(mp3_filename, 'rb') as f_mp3:
+    # with open(mp3_filename, 'rb') as f_mp3:
     #    pt = f_mp3.read()
-    pt=mp3_filename
-
+    pt = mp3_filename
     #a random 128 bit initial vector
     iv = base64.b16encode(random.getrandbits(128).to_bytes(16, byteorder='little'))
 
     #encryption
+    string_value = str(shared_secret_key_alice.x.n)
+    bytes_value = string_value.encode('utf-8')
+
     leaCBC = LEA.CBC(True, point_to_bytes(shared_secret_key_alice),iv,True)
     ct = leaCBC.update(pt)
     ct += leaCBC.final()
@@ -133,21 +128,19 @@ def main():
     print("Alice signs with her secret key using schnorr signature")
     ct_bytes = sha256(ct)
     #changed int.to_bytes(alice_secret_key)
-
-    hex_string =hex(alice_secret_key)
-    byte_string = bytes.fromhex(hex_string[2:])              #removed 0x
-    byte_string=binascii.hexlify(byte_string).decode('utf-8')#convert the byte string to its hexadecimal representation
-    sig = sl.schnorr_sign(ct_bytes, byte_string)
+    hex_privkey = hex(alice_secret_key).replace('0x', '').rjust(64, '0')
+    sig = sl.schnorr_sign(ct_bytes, hex_privkey)
 
     print("Alice sends sig and ct to Bob")
     print("Bob verifies the signature")
     #3s = str(alice_public_key.x.n)
-    alice_public_key_bytes = point_to_bytes(alice_public_key)
-    verify = sl.schnorr_verify(ct_bytes, alice_public_key_bytes, sig)
+
+    pubkey_bytes = bytes.fromhex(alice_public_key)
+    verify = sl.schnorr_verify(ct_bytes, pubkey_bytes, sig)
     if verify:
         print("The signature is VALID for this message and this public key")
         print("Bob decrypts the message that Alice sent")
-        leaCBC = LEA.CBC(False, point_to_bytes(sharedSecret1), iv, True)
+        leaCBC = LEA.CBC(False, point_to_bytes(shared_secret_key_bob), iv, True)
         pt = leaCBC.update(ct)
         pt += leaCBC.final()
 
@@ -157,8 +150,6 @@ def main():
 
     else:
         print("The signature is NOT VALID for this message and this public key")
-
-    print("Bob encrypts the LEA key to send to Alice using the Alice public key")
 
 if __name__ == "__main__":
     main()
